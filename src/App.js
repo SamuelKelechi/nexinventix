4546bb5705e249985d6d052eaf5c724633ee998c
SamuelKelechi <kcfreshie@ymail.com>
2024-12-12 16:44:40 -0800

    first set up commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './nex-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="#"
+          rel="noopener noreferrer"
+        >
+          NEXINVENTIX TECHNOLOGY
+        </a>
+        <p>
+          COMING SOON...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
